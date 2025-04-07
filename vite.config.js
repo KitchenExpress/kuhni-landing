c056ec82cf8a37a25fe3479abbd34db6e1abde17
KitchenExpress <kuhni-ykt@yandex.ru>
2025-04-07 18:48:32 +0900

    build: обновление сайта

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  base: '/kuhni-landing/', // <--- ВАЖНО
+  base: '/kuhni-landing/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });

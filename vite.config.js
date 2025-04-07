9677d7d94e24a771e594a065e112e9cb64859483
KitchenExpress <kuhni-ykt@yandex.ru>
2025-04-07 14:07:38 +0900

    Добавил base в vite.config.js для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/kuhni-landing/', // 👈 добавь это!
   plugins: [react()],
   resolve: {
     alias: {

323dce9d3032611282ad689b180e6749d0a39b20
KitchenExpress <kuhni-ykt@yandex.ru>
2025-04-08 10:25:46 +0900

    Deploy

--- src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ui/card.jsx
-export function Card({ children, className }) {
-    return <div className={`bg-white rounded-xl shadow ${className}`}>{children}</div>;
-  }
-  
-  export function CardContent({ children, className }) {
-    return <div className={className}>{children}</div>;
-  }
-  
